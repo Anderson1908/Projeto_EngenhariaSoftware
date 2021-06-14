7bd0684ccece64957c17fe2d8a1c0a921e035c3f
Anderson1908 <83255606+Anderson1908@users.noreply.github.com>
2021-06-14 17:57:21 -0300

    Diagrama de classes e modelagem de classes em java

---
+++ implementacao/Sistema_Entrega/src/sistema_entrega/Gestao.java
@@ -0,0 +1,21 @@
+package sistema_entrega;
+
+public class Gestao {
+    private Cliente cliente;
+    
+    public void inserir(){
+        
+    }
+    
+    public void atualizar(){
+        
+    }
+    
+    public void excluir(){
+        
+    }
+    
+    public void consultar(){
+        
+    }
+}
